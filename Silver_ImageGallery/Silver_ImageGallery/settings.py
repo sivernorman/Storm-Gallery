e207b97871fd0cde5b7adb28b941a6b1d1cf4829
sivernorman <silvernorman.o@gmail.com>
2022-02-28 13:01:43 +0300

    update app

--- Silver_ImageGallery/Silver_ImageGallery/settings.py
+++ Silver_ImageGallery/Silver_ImageGallery/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 import django_heroku
-django_heroku.settings(locals())
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

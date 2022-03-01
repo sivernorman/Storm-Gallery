8b59e7aec0022036c6827ad244a1e78e407e91c5
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-03-01 21:53:26 +0300

    Code refactor

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Desktop/python-projects/Storm-Gallery/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Desktop/python-projects/Storm-Gallery/Silver_ImageGallery/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

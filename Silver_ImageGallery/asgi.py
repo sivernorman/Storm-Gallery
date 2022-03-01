e25bbe8df183e52a95e7171929acab1e0350234f
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-03-01 22:42:10 +0300

    Production settings

--- Silver_ImageGallery/asgi.py
+++ Silver_ImageGallery/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Silver_ImageGallery.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Silver_ImageGallery.settings.prod')
 
 application = get_asgi_application()

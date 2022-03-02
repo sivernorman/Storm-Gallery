66555177c254ebc4843f39103fb6e691bd229ec4
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-03-02 07:25:37 +0300

    Database config

--- Silver_ImageGallery/settings/prod.py
+++ Silver_ImageGallery/settings/prod.py
@@ -4,27 +4,27 @@ from Silver_ImageGallery.settings.local import *
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'silverImage',
-#         'USER': 'postgres',
-#         'PASSWORD': 'silverImage',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "silverImage",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'silverImage',
         'USER': 'postgres',
         'PASSWORD': 'silverImage',
         'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': "silverImage",
+#         'USER': 'postgres',
+#         'PASSWORD': 'silverImage',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # SSL and TLS settings
